6251ebd4938eab05ba57bd3e98629334510ac8f5
trustmaster <trustmaster@kodigy.com>
2012-03-22 20:53:44 +0300

    Apostrophes in langfile

--- lang/en/orm.en.lang.php
+++ lang/en/orm.en.lang.php
@@ -1,9 +1,9 @@
 <?php
 
 $L['InvalidColumnName'] = 'Invalid column name: {$column}';
-$L['CantUpdateAutoIncrementColumn'] = 'Cant update auto_increment column: {$column}';
-$L['CantUpdatePrimaryKeyColumn'] = 'Cant update primary_key column: {$column}';
-$L['CantUpdateLockedColumn'] = 'Cant update locked column: {$column}';
+$L['CantUpdateAutoIncrementColumn'] = 'Can\'t update auto_increment column: {$column}';
+$L['CantUpdatePrimaryKeyColumn'] = 'Can\'t update primary_key column: {$column}';
+$L['CantUpdateLockedColumn'] = 'Can\'t update locked column: {$column}';
 $L['ValueIsBelowMinimumLength'] = 'Value of "{$column}" with length {$length} is below minimum length of {$minlength}.';
 $L['ValueExceedsMaximumLength'] = 'Value of "{$column}" with length {$length} exceeds maximum length of {$maxlength}.';
 $L['InvalidVariableType'] = 'Invalid variable type ({$type}) for column {$column}.';
